63c9d1547c1ce99ad2c544373e6e7c534ffe63d0
Kien Nguyen <kienbinance15@gmail.com>
2025-12-03 14:09:09 +0700

    Initial commit

---
+++ src/components/client/footer.client.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer style={{ padding: 15, textAlign: "center" }}>
+            {/* <div>Frontend React Typescript - Series Nest.JS Basic &copy; Hỏi Dân IT</div> */}
+        </footer>
+    );
+};
+
+export default Footer;
